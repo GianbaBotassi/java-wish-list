49be7ae21aa4fa93aaf96c34e50a20a53b8e8d1c
GianbaBotassi <gianba89@gmail.com>
2023-10-26 16:44:02 +0200

    Change dir

--- src/org/lessons/java/listaRegali/Main.java
+++ src/org/lessons/java/listaRegali/Main.java
@@ -1,7 +1,6 @@
-package org.lessons.java;
+package org.lessons.java.listaRegali;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
